6ff3d1d046f0347cc7887ffbe4817af43217687a
kleberlimagit <mov_kleber@hotmail.com>
2023-07-12 14:33:11 -0300

    component input - useTheme

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,13 @@
+import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components"; 
+import { Container } from "./styles";
+
+
+export function Input({...rest} : TextInputProps) {
+  const {COLORS} = useTheme();
+
+
+  return (
+      <Container {...rest} placeholderTextColor={COLORS.GRAY_300}/>
+    )
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export default function NewGroup(){
     return (
@@ -10,7 +11,8 @@ export default function NewGroup(){
             <Content>
                 <Icon/>
                 <Highlight title="Novo Time" subtitle="crie a turma para adicionar os jogadores"/>
-                <Button title="Criar"/>
+                <Input placeholder="Nome do time"/>
+                <Button title="Criar" style={{marginTop: 20}}/>
             </Content>
         </Container>
     )
